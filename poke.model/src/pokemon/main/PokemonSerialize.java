6dd78d56740ce4050d7daa39e8668ca61c7fc8b6
torsrex <rex.datamaskin@gmail.com>
2018-11-21 11:46:48 +0100

    Nüuu köyrer më

--- poke.model/src/pokemon/main/PokemonSerialize.java
+++ poke.model/src/pokemon/main/PokemonSerialize.java
@@ -11,6 +11,7 @@ import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import pokemon.Pokemon;
 import pokemon.PokemonFactory;
 import pokemon.PokemonPackage;
+import pokemon.Root;
 
 public class PokemonSerialize {
 	public static void main(String[] args) {
@@ -19,17 +20,26 @@ public class PokemonSerialize {
 		
 		PokemonFactory factory = PokemonFactory.eINSTANCE;
 		
-		Pokemon pokemon = factory.createPokemon();
-		pokemon.setName("Pikasjku");
+		Root root = factory.createRoot();
+		
+		
+		for(int i = 0; i<5; i++) {
+			Pokemon pokemon = factory.createPokemon();
+			pokemon.setName(Integer.toString(i));
+			root.getPokemon().add(pokemon);
+			
+		}
+		
 		
 		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
 		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		
+		// The string is the package in the ecore model
 		m.put("pokemon", new XMIResourceFactoryImpl());
 		
 		ResourceSet resSet = new ResourceSetImpl();
-		
-		Resource resource = resSet.createResource(URI.createURI("model/mm1m.pokemon"));
-		resource.getContents().add(pokemon);
+		Resource resource = resSet.createResource(URI.createURI("model/m1mm.pokemon"));
+		resource.getContents().add(root);
 		try {
 			resource.save(Collections.EMPTY_MAP);
 			
